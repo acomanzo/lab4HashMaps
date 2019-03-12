9b77bc0de9564813dfa7febd67901d4d2f11ee99
ednut54 <48428026+ednut54@users.noreply.github.com>
2019-03-12 13:49:04 -0400

    Improved readability of User.java
    Added javaDoc comments and closed a resource leak.

--- User.java
+++ User.java
@@ -3,6 +3,9 @@ package lab4;
 import java.util.Date;
 import java.util.Scanner;
 
+/**
+ * Models a user and its attributes.
+ */
 public class User {
 	
 	private String firstName;
@@ -21,18 +24,42 @@ public class User {
 	
 	private static int IDCount = 0;
 	
+	/**
+	 * Constructs a User object with a first and last name, email, and password.
+	 * @param firstName A String representing the first name of the user
+	 * @param lastName A String representing the last name of the user
+	 * @param email A String representing the email address of the user
+	 * @param password A String representing the user's password
+	 */
 	public User(String firstName, String lastName, String email, String password) {
 		this(firstName, lastName, email, password, null);
 		IDCount++;
 		userID = IDCount;
 	}
 	
+	/**
+	 * Constructs a User Object with a first and last name, email address, password, and gender. 
+	 * @param firstName A String representing the first name of the user
+	 * @param lastName A String representing the last name of the user
+	 * @param email A String representing the email address of the user
+	 * @param password A String representing the user's password
+	 * @param gender A String representing the user's gender
+	 */
 	public User(String firstName, String lastName, String email, String password, String gender) {
 		this(firstName, lastName, email, password, gender, null);
 		IDCount++;
 		userID = IDCount;
 	}
 	
+	/**
+	 * Constructs a User Object with a first and last name, email address, password, gender, and birthdate. 
+	 * @param firstName A String representing the first name of the user
+	 * @param lastName A String representing the last name of the user
+	 * @param email A String representing the email address of the user
+	 * @param password A String representing the user's password
+	 * @param gender A String representing the user's gender
+	 * @param birthdate A Date object representing the birth date of the user
+	 */
 	public User(String firstName, String lastName, String email, String password, String gender, Date birthdate) {
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -44,38 +71,73 @@ public class User {
 		userID = IDCount;
 	}
 	
+	/**
+	 * Returns the first and last name of the user.
+	 * @return A String representing the first and last name of the user
+	 */
 	public String getName() {
-		return firstName + lastName;
+		return firstName + " " + lastName;
 	}
 	
+	/**
+	 * Returns the email address of the user.
+	 * @return A String representing the email address of the user
+	 */
 	public String getEmail() {
 		return email;
 	}
 	
+	/**
+	 * Returns the ID number of the user.
+	 * @return An integer representing the ID number of the user
+	 */
 	public int getUserID() {
 		return userID;
 	}
 	
+	/**
+	 * Sets the first name of the user.
+	 * @param firstName A String representation of the user's first name
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 	
+	/**
+	 * Sets the last name of the user.
+	 * @param lastName A String representation of the user's last name
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 	
+	/**
+	 * Sets the email address of the user.
+	 * @param email A String representation of the user's email address
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 	
+	/**
+	 * Sets the password of the user.
+	 * @param password A String representation of the user's password
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
 	
+	/**
+	 * Sets the gender of the user.
+	 * @param password A String representation of the user's gender
+	 */
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
 	
+	/**
+	 * Sets the security questions of the user.
+	 */
 	public void setSecurityQuestions() {
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Enter the first question.");
@@ -87,5 +149,7 @@ public class User {
 		securityQuestion2 = keyboard.nextLine();
 		System.out.println("Enter the answer to the second question.");
 		securityQuestion2Answer = keyboard.nextLine();
+		
+		keyboard.close();
 	}
 }
